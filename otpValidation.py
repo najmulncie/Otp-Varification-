bbe2ec8809032f5f36967ce8d9b5c1d10b624265
najmulnice-Nice284 <najmulnice284@gmail.com>
2022-11-23 19:45:10 +0600

    email otp varification

---
+++ otpValidation.py
@@ -0,0 +1,24 @@
+import os
+import math
+import random
+import smtplib
+
+digits = "0123456789"
+OTP = ""
+for i in range(6):
+    OTP += digits[math.floor(random.random()*10)]
+otp = OTP + " is your OTP"
+msg = otp
+
+
+
+s = smtplib.SMTP('smtp.gmail.com', 587)
+s.starttls()
+s.login('najmulnice284@gmail.com', "yckrrlanchtxvoqc")
+emailid = input("Enter your email: ")
+s.sendmail('yckrrlanchtxvoqc',emailid,msg)
+a = input("Enter Your OTP >>: ")
+if a == OTP:
+    print("Verified")
+else:
+    print("Please Check your OTP again")
